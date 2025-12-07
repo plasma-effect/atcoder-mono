14e4d51f1861d6ecb5c91165842c04889b2e56c6
plasma-ep <mk_vertex3625_plasma@kcn.jp>
2025-12-07 17:48:12 +0900

    change statid_dual_array

--- include/competitive/container/static_dual_array.hpp
+++ include/competitive/container/static_dual_array.hpp
@@ -4,66 +4,62 @@
 #include <bits/stdc++.h>
 
 namespace common {
-template <typename T, std::size_t H, std::size_t W> class static_dual_array {
+template <typename T, int H, int W> class static_dual_array {
   std::array<T, W * H> inside_;
 
 public:
   static_dual_array() : inside_{} {};
-  template <std::integral Int0, std::integral Int1>
-  T& operator()(Int0 i0, Int1 i1, CL_FROM_LOCATION) {
-    CL_ASSERT(std::cmp_greater_equal(i0, 0) && std::cmp_less(i0, H));
-    CL_ASSERT(std::cmp_greater_equal(i1, 0) && std::cmp_less(i1, W));
-    return inside_[i0 * W + i1];
+  T& operator()(common::argument<int> i0, common::argument<int> i1) {
+    CL_ASSERT(0 <= i0.value && i0.value < H);
+    CL_ASSERT(0 <= i1.value && i1.value < W);
+    return inside_[i0.value * W + i1.value];
   }
-  template <std::integral Int0, std::integral Int1>
-  T const& operator()(Int0 i0, Int1 i1, CL_FROM_LOCATION) const {
-    CL_ASSERT(std::cmp_greater_equal(i0, 0) && std::cmp_less(i0, H));
-    CL_ASSERT(std::cmp_greater_equal(i1, 0) && std::cmp_less(i1, W));
-    return inside_[i0 * W + i1];
+  T const& operator()(common::argument<int> i0,
+                      common::argument<int> i1) const {
+    CL_ASSERT(0 <= i0.value && i0.value < H);
+    CL_ASSERT(0 <= i1.value && i1.value < W);
+    return inside_[i0.value * W + i1.value];
   }
-  template <std::integral Int0, std::integral Int1> T& at(Int0 i0, Int1 i1) {
-    if (std::cmp_less(i0, 0) || std::cmp_greater_equal(i0, H)) [[unlikely]] {
+  T& at(int i0, int i1) {
+    if (i0 < 0 || i0 >= H) [[unlikely]] {
       throw std::out_of_range(
           "argument 1 of static_dual_array::at is out of range");
-    } else if (std::cmp_less(i1, 0) || std::cmp_greater_equal(i1, W))
-        [[unlikely]] {
+    } else if (i1 < 0 || i1 >= W) [[unlikely]] {
       throw std::out_of_range(
           "argument 2 of static_dual_array::at is out of range");
     }
     return inside_[i0 * W + i1];
   }
-  template <std::integral Int0, std::integral Int1>
-  T const& at(Int0 i0, Int1 i1) const {
-    if (std::cmp_less(i0, 0) || std::cmp_greater_equal(i0, H)) [[unlikely]] {
+  T const& at(int i0, int i1) const {
+    if (i0 < 0 || i0 >= H) [[unlikely]] {
       throw std::out_of_range(
           "argument 1 of static_dual_array::at is out of range");
-    } else if (std::cmp_less(i1, 0) || std::cmp_greater_equal(i1, W))
-        [[unlikely]] {
+    } else if (i1 < 0 || i1 >= W) [[unlikely]] {
       throw std::out_of_range(
           "argument 2 of static_dual_array::at is out of range");
     }
     return inside_[i0 * W + i1];
   }
 
-  constexpr std::pair<std::size_t, std::size_t> dimensions() const {
+  constexpr std::pair<int, int> dimensions() const {
     return {H, W};
   }
-  constexpr std::size_t size() const {
+  constexpr int size() const {
     return H * W;
   }
 };
-template <typename T, std::size_t H, std::size_t W>
+template <typename T, int H, int W>
 void operator<<(internal::print_base_t& pb,
                 static_dual_array<T, H, W> const& ar) {
   const auto [prefix, suffix, delim] = pb.get_range_decolater();
   const char* outer_delim = "";
   pb << prefix;
-  for (auto i = 0uz; i < H; ++i) {
+  for (int i = 0; i < H; ++i) {
     pb << std::exchange(outer_delim, delim);
     const char* inner_delim = prefix;
-    for (auto j = 0uz; j < W; ++j) {
+    for (int j = 0; j < W; ++j) {
       pb << std::exchange(inner_delim, delim);
-      pb << ar(i, j, DEFAULT_LOCATION);
+      pb << ar(i, j);
     }
     pb << suffix;
   }
