cc7e5b80a9309a26c1c05b95707268825a0a87ac
plasma-ep <mk_vertex3625_plasma@kcn.jp>
2025-12-11 11:50:59 +0900

    add ignore macro

--- include/competitive/utility.hpp
+++ include/competitive/utility.hpp
@@ -9,3 +9,6 @@ template <typename T> constexpr auto max_v = std::numeric_limits<T>::max();
 template <typename T> constexpr auto min_v = std::numeric_limits<T>::min();
 template <typename T> using pair = std::pair<T, T>;
 } // namespace common
+#define IGNORE_CAT_I2(a, b) a##b
+#define IGNORE_CAT_I(a, b) IGNORE_CAT_I2(a, b)
+#define IGNORE [[maybe_unused]] auto IGNORE_CAT_I(_ignore, __COUNTER__)
